b86beb12a5fb6d56348cb3717ba4416125b5eff0
Robbie Pitts <robbie@sweatypitts.com>
2013-07-19 20:47:36 -0400

    Update run clear and close behavior

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :require_login
   
   def show
-    @orders = Order.includes(:user).today.in_organization(current_organization).unaccepted.order("created_at DESC").all
+    @orders = Order.includes(:user).today.in_organization(current_organization).unaccepted.order("created_at DESC")
     @gofer_run = current_user.gofer_runs.in_organization(current_organization).pending.first
   end
 end
--- app/controllers/gofer_runs_controller.rb
+++ app/controllers/gofer_runs_controller.rb
@@ -4,13 +4,13 @@ class GoferRunsController < ApplicationController
   def clear
     @gofer_run = GoferRun.find(params[:id])
     @gofer_run.clear_orders
-    @orders = Order.includes(:user).unaccepted.order("created_at DESC").all
+    @orders = Order.includes(:user).today.in_organization(current_organization).unaccepted.order("created_at DESC")
   end
   
   def close
     @gofer_run = GoferRun.find(params[:id])
     @gofer_run.close
-    @orders = Order.includes(:user).unaccepted.order("created_at DESC").all
+    @orders = Order.includes(:user).today.in_organization(current_organization).unaccepted.order("created_at DESC")
   end
   
 end
